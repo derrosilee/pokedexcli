149ca30c136676df16d648d13c031d3a6b5f4a39
derrosilee <derrossilee@gmail.com>
2024-02-02 06:56:31 +0300

    Implement basic command structure in REPL
    Added a basic command structure in the repl.go REPL loop for better interaction. "help" and "exit" functions have been introduced, including default responses for invalid commands. Additionally, incorporated command objects with name, description, and callback properties for further expansion.

--- repl.go
+++ repl.go
@@ -20,10 +20,42 @@ func startRepl() {
 		if len(cleaned) == 0 {
 			continue
 		}
-
-		fmt.Println("echoing: ", cleaned)
+		command := cleaned[0]
+		switch command {
+		case "help":
+			fmt.Println("Welcome to Pokedex help menu!")
+			fmt.Println("Here are your available commands:")
+			fmt.Println(" - help")
+			fmt.Println(" - exit")
+			fmt.Println("")
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid Command")
+		}
 	}
 
+}
+
+type cliCommands struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func getCommands() map[string]cliCommands {
+	return map[string]cliCommands{
+		"help": {
+			name: "help",
+			description: "Prints the help menu",
+			callback: ,
+		},
+		"exit": {
+			name: "exit",
+			description: "Turns of the Pokedex",
+			callback: ,
+		},
+	}
 }
 
 func cleanInput(str string) []string {
